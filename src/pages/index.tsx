1a0c6b36f1c645eacfc1c6f602d284b3645ee66a
Andrea Gorletta <andrea.alice.gorletta@gmail.com>
2025-09-18 14:30:26 +0200

    cleaning up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import dynamic from "next/dynamic";
 
 export default function Home() {
   return (
@@ -12,6 +11,10 @@ export default function Home() {
           name="description"
           content="A simple Next.js application displaying a title and description"
         />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, interactive-widget=resizes-content"
+        ></meta>
       </Head>
       <div className={styles.container}>
         <h1 className={styles.title}>Buildo Next.js App</h1>
